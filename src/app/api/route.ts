827f3b7b76bd990138fe0f29580abffe304d1060
Jaynel Patiarba <lenyajpatiarba@gmail.com>
2025-03-07 14:03:39 +0800

    adding regex

---
+++ src/app/api/route.ts
@@ -0,0 +1,23 @@
+import { nuclei_regex } from "@/constants/constants";
+import { NextRequest } from "next/server";
+
+export function get_secret(data: string): string[] {
+    const result: string[] = [];
+    for (let i = nuclei_regex.length - 1; i >= 0; i--) {
+      const tmp_result = data.match(nuclei_regex[i]);
+      if (tmp_result !== null) {
+        for (const match of tmp_result) {
+          result.push(match);
+        }
+      }
+    }
+    return result;
+  }
+
+  export async function POST(req: NextRequest) {
+    const { data } = await req.json();
+    const secret = get_secret(data);
+    return new Response(JSON.stringify(secret), {
+      headers: { "content-type": "application/json" },
+    });
+  }
