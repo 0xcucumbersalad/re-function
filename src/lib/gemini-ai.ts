e831482c2154a6bf461661771e02f6d0d1ce853e
Jaynel Patiarba <lenyajpatiarba@gmail.com>
2025-03-07 21:33:09 +0800

    adding ai fine shey

--- src/lib/gemini-ai.ts
+++ src/lib/gemini-ai.ts
@@ -1,4 +1,4 @@
-import { GoogleGenerativeAI, SchemaType, ObjectSchema } from "@google/generative-ai";
+import { GoogleGenerativeAI, ObjectSchema } from "@google/generative-ai";
 
 const apiKey = process.env.GOOGLE_API_KEY || "";
 const genAI = new GoogleGenerativeAI(apiKey);
